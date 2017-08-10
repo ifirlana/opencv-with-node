11d7befbe1989afb795a4d4766388a4399d61146
ikhlas firlana <ifirlana@gmail.com>
2017-08-10 23:02:10 +0700

    no message

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var ktp = require("./lib/ktp-detection");
 var start = new ktp();
-start.detect("./samples/people.jpg", "./result/people-detect.png");
+start.detect("./samples/people-02.jpg", "./result/people-detect.png");
